10bac5cc2e046c7abc89ed62f81ab26a2a4a83be
Amaderu <ilahavele454@gmail.com>
2020-10-14 16:59:41 +0700

    Переделанно окно авторизации Добавлена валидация полей
    Signed-off-by: Mikhail <Bereberdin>

--- app/src/main/java/com/example/firstproject/MainActivity2.java
+++ app/src/main/java/com/example/firstproject/MainActivity2.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
@@ -14,13 +15,109 @@ import java.util.regex.Pattern;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private EditText textInputEmail;
+    private EditText textInputUsername;
+    private EditText textInputPassword;
+    private EditText textInputConPassword;
+
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    //"(?=.*[0-9])" +         //at least 1 digit
+                    //"(?=.*[a-z])" +         //at least 1 lower case letter
+                    //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                    "(?=.*[a-zA-Z])" +      //any letter
+                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                    "(?=\\S+$)" +           //no white spaces
+                    ".{4,}" +               //at least 4 characters
+                    "$");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textInputEmail = findViewById(R.id.fieldMail);
+        textInputUsername = findViewById(R.id.fieldFName);
+        textInputPassword = findViewById(R.id.fieldPass);
+        textInputConPassword =findViewById(R.id.fieldConPass);
+        auto();
+
+    }
+    private void auto(){
+        final EditText FName = findViewById(R.id.fieldFName);
+        final EditText SName = findViewById(R.id.fieldSName);
+        final EditText Mail = findViewById(R.id.fieldMail);
+        final EditText Pass = findViewById(R.id.fieldPass);
+        final EditText ConPass = findViewById(R.id.fieldConPass);
+
+        FName.setText("Amaderu");
+        SName.setText("454");
+        Mail.setText("Qwe@mail.com");
+        Pass.setText("QQq#1234");
+        ConPass.setText("QQq#1234");
     }
 
-    public void  onClick(View v){
+    private boolean validateEmail() {
+        String emailInput = textInputEmail.getText().toString().trim();
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textInputEmail.setError("Please enter a valid email address");
+            return false;
+        } else {
+            textInputEmail.setError(null);
+            return true;
+        }
+    }
+    private boolean validateUsername() {
+        String usernameInput = textInputUsername.getText().toString().trim();
+        if (usernameInput.isEmpty()) {
+            textInputUsername.setError("Field can't be empty");
+            return false;
+        } else if (usernameInput.length() > 18) {
+            textInputUsername.setError("Username too long");
+            return false;
+        } else {
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getText().toString().trim();
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        }else if (passwordInput.length() < 8) {
+            textInputPassword.setError("Password is short");
+            return false;
+        }else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            textInputPassword.setError("Password too weak");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+    private boolean validateConPassword(){
+        String passwordInput = textInputPassword.getText().toString().trim();
+        String ConPasswordInput = textInputConPassword.getText().toString().trim();
+        if(!passwordInput.equals(ConPasswordInput)){
+            textInputConPassword.setError("Password is not match");
+            return false;
+        }
+        else{
+            textInputConPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmI(View v) {
+        if (!validateEmail() | !validateUsername() | !validatePassword()) {
+            return;
+        }
+    }
+
+    public void  confirmInput(View v){
         final EditText FName = findViewById(R.id.fieldFName);
         final EditText SName = findViewById(R.id.fieldSName);
         final EditText Mail = findViewById(R.id.fieldMail);
@@ -32,21 +129,41 @@ public class MainActivity2 extends AppCompatActivity {
         intent.putExtra("SecondName",SName.getText().toString());
         intent.putExtra("Mail",Mail.getText().toString());
         String mail =Mail.getText().toString();
-        Pattern pattern1 = Pattern.compile("([A-Za-z]+)(\\d*)(@mail\\.ru)");
-        Pattern pattern2 = Pattern.compile("([\\D]*)(@mail\\.ru)");
-        boolean bmail = mail.matches(pattern1.toString());
-        if(!bmail){
+        //Pattern pattern1 = Pattern.compile("([A-Za-z]+)(\\d*)(@mail\\.ru)");
+        //Pattern pattern2 = Pattern.compile("([\\D]*)(@mail\\.ru)");
+        //Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+        //boolean bmail = mail.matches(pattern1.toString());
+
+
+        /*if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
             Toast toast = Toast.makeText(getApplicationContext(), "НЕ правильно",
                     Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
-            Mail.setTextColor(Color.RED);
+            if(Mail.getText().length()==0)
+                Mail.setHintTextColor(Color.RED);
+            else Mail.setTextColor(Color.RED);
             return;
         }
+        Mail.setHintTextColor(Color.BLACK);
         Mail.setTextColor(Color.BLACK);
         String passsword=Pass.getText().toString();
         String conPass=ConPass.getText().toString();
         if(passsword.equals(conPass) && !passsword.isEmpty() && !conPass.isEmpty())
-            startActivity(intent);
+            startActivity(intent);*/
+
+        if (!validateEmail() | !validateUsername() | !validatePassword() | !validateConPassword()) {
+            return;
+        }
+        startActivity(intent);
+        finish();
+
+    }
+
+
+    public void Checking(EditText EdT){
+        if(EdT.getText().length()==0)
+            EdT.setHintTextColor(Color.RED);
+        else EdT.setTextColor(Color.BLACK);
     }
 }
